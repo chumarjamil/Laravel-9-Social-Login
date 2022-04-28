6d065f06b4662e4cdca220d29b9043a27955e7fa
Umar Jamil <umar@emotiv.com>
2022-04-28 21:51:40 +0800

    Laravel 9 Social Login code

---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::get('/auth/redirect', function () {
+    return \Laravel\Socialite\Facades\Socialite::driver('github')->redirect();
+});
+
+Route::get('/auth/callback', function () {
+    $githubUser = \Laravel\Socialite\Facades\Socialite::driver('github')->user();
+
+   $user = \App\Models\User::updateOrCreate ([
+       'github_id' => $githubUser->id
+   ],[
+        'name' => $githubUser->name,
+        'email' => $githubUser->email,
+        'github_token' => $githubUser->token,
+        'github_refresh_token' => $githubUser->refreshToken,
+    ]);
+
+   \Illuminate\Support\Facades\Auth::login($user);
+
+   return redirect('dashboard');
+});
+
+require __DIR__.'/auth.php';
